a119d603c74eb8fe9498263d00302290f73cd1bd
ElyasafSinvani <94112241+ElyasafSinvani@users.noreply.github.com>
2021-11-11 15:39:07 +0200

    Ver1

--- Project12/Main1.c
+++ Project12/Main1.c
@@ -2,9 +2,5 @@
 
 int main()
 {
-	printf("Hello");
-	printf("Hello2");
-	printf("HelloElyasaf");
-	printf("Hello judy");
-	printf("Hello jdy2");
+	printf("Hello\nMy name is Elyasaf and im the master\n");
 }
